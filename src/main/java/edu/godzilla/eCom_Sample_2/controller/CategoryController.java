008136297c916331e3500c43334db1710d0c91c9
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-02 11:40:05 +0530

    Created the createCategory method

--- src/main/java/edu/godzilla/eCom_Sample_2/controller/CategoryController.java
+++ src/main/java/edu/godzilla/eCom_Sample_2/controller/CategoryController.java
@@ -1,9 +1,7 @@
 package edu.godzilla.eCom_Sample_2.controller;
 
 import edu.godzilla.eCom_Sample_2.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,11 +9,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
-    private List<Category> getAllCategories = new ArrayList<>();
+    private List<Category> allCategories = new ArrayList<>();
 
     @GetMapping("/public/categories")
     public List<Category> getAllCategories(){
-        return getAllCategories;
+        return allCategories;
+    }
+
+    @PostMapping("/admin/category")
+    public String createCategory(@RequestBody Category category){
+        allCategories.add(new Category(category.getId(), category.getName()));
+        return "Category created successfully!";
     }
 
 }
