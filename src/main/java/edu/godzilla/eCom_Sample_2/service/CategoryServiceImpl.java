33b03836675917f9b0380175726b383ccbf7dc9c
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-02 12:13:32 +0530

    Decorated the CategoryServiceImpl using the @Service annotation

--- src/main/java/edu/godzilla/eCom_Sample_2/service/CategoryServiceImpl.java
+++ src/main/java/edu/godzilla/eCom_Sample_2/service/CategoryServiceImpl.java
@@ -1,10 +1,12 @@
 package edu.godzilla.eCom_Sample_2.service;
 
 import edu.godzilla.eCom_Sample_2.model.Category;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CategoryServiceImpl implements CategoryService{
     private List<Category> allCategories = new ArrayList<>();
 
