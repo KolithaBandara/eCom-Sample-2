42887febbeee627d036ebeb4b93fca2e72adb376
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-02 11:16:33 +0530

    initialized the project

---
+++ src/main/java/edu/godzilla/eCom_Sample_2/EComSample2Application.java
@@ -0,0 +1,13 @@
+package edu.godzilla.eCom_Sample_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComSample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComSample2Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/godzilla/eCom_Sample_2/EComSample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.godzilla.eCom_Sample_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComSample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
