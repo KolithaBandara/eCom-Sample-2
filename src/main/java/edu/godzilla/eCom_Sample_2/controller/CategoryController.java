adf1c2dfb0237056841b0a95635d307b989109b0
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-02 14:46:53 +0530

    Decoarate the categoryService reference variable using @Autowired annotation

--- src/main/java/edu/godzilla/eCom_Sample_2/controller/CategoryController.java
+++ src/main/java/edu/godzilla/eCom_Sample_2/controller/CategoryController.java
@@ -3,6 +3,7 @@ package edu.godzilla.eCom_Sample_2.controller;
 import edu.godzilla.eCom_Sample_2.model.Category;
 import edu.godzilla.eCom_Sample_2.service.CategoryService;
 import edu.godzilla.eCom_Sample_2.service.CategoryServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,11 +11,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
+    @Autowired
     private CategoryService categoryService;
-
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
 
     @GetMapping("/public/categories")
     public List<Category> getAllCategories(){
