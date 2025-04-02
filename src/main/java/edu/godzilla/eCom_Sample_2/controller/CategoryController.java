6cf2499ba9ccb84d2bdc3ef7cd8b2b3240c2e5b5
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-02 11:24:41 +0530

    Created the getAllCategories method

--- src/main/java/edu/godzilla/eCom_Sample_2/controller/CategoryController.java
+++ src/main/java/edu/godzilla/eCom_Sample_2/controller/CategoryController.java
@@ -1,5 +1,21 @@
 package edu.godzilla.eCom_Sample_2.controller;
 
+import edu.godzilla.eCom_Sample_2.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class CategoryController {
+    List<Category> allCategories = new ArrayList<>();
+
+    @GetMapping("/public/categories")
+    public List<Category> getAllCategories(){
+        return allCategories;
+    }
 
 }
