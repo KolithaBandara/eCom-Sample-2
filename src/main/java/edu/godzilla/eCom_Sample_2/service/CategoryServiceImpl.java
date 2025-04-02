42ed2bb189822baf3d0f3f84f26fad11f0aa0ce4
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-02 12:00:40 +0530

    Created the CategoryServiceImpl to implements the CategoryService interface and implenemented the methods from CategoryService

---
+++ src/main/java/edu/godzilla/eCom_Sample_2/service/CategoryServiceImpl.java
@@ -0,0 +1,17 @@
+package edu.godzilla.eCom_Sample_2.service;
+
+import edu.godzilla.eCom_Sample_2.model.Category;
+
+import java.util.List;
+
+public class CategoryServiceImpl implements CategoryService{
+    @Override
+    public List<Category> getAllCategories() {
+        return List.of();
+    }
+
+    @Override
+    public String createCategory(Category category) {
+        return "";
+    }
+}
