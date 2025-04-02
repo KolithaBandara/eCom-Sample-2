75282a8ddf93a097378884678bd2c8283cebf78e
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-02 12:04:53 +0530

    Modified the CategoryServiceImpl and CategoryController class

--- src/main/java/edu/godzilla/eCom_Sample_2/controller/CategoryController.java
+++ src/main/java/edu/godzilla/eCom_Sample_2/controller/CategoryController.java
@@ -1,25 +1,25 @@
 package edu.godzilla.eCom_Sample_2.controller;
 
 import edu.godzilla.eCom_Sample_2.model.Category;
+import edu.godzilla.eCom_Sample_2.service.CategoryService;
+import edu.godzilla.eCom_Sample_2.service.CategoryServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
-    private List<Category> allCategories = new ArrayList<>();
+    CategoryService categoryService = new CategoryServiceImpl();
 
     @GetMapping("/public/categories")
     public List<Category> getAllCategories(){
-        return allCategories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/admin/category")
     public String createCategory(@RequestBody Category category){
-        allCategories.add(new Category(category.getId(), category.getName()));
-        return "Category created successfully!";
+        return categoryService.createCategory(category);
     }
 
 }
--- src/main/java/edu/godzilla/eCom_Sample_2/service/CategoryServiceImpl.java
+++ src/main/java/edu/godzilla/eCom_Sample_2/service/CategoryServiceImpl.java
@@ -2,16 +2,20 @@ package edu.godzilla.eCom_Sample_2.service;
 
 import edu.godzilla.eCom_Sample_2.model.Category;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryServiceImpl implements CategoryService{
+    private List<Category> allCategories = new ArrayList<>();
+
     @Override
     public List<Category> getAllCategories() {
-        return List.of();
+        return allCategories;
     }
 
     @Override
     public String createCategory(Category category) {
-        return "";
+        allCategories.add(category);
+        return "Category Created Successfully!";
     }
 }
