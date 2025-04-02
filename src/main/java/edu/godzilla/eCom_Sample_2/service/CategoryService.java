d97a7d51308f94876fd5020ccb6d31a377d0972c
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-02 11:58:42 +0530

    Use loosecoupling with CategoryService interface

--- src/main/java/edu/godzilla/eCom_Sample_2/service/CategoryService.java
+++ src/main/java/edu/godzilla/eCom_Sample_2/service/CategoryService.java
@@ -1,4 +1,11 @@
 package edu.godzilla.eCom_Sample_2.service;
 
+import edu.godzilla.eCom_Sample_2.model.Category;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public interface CategoryService {
+    List<Category> getAllCategories();
+    String createCategory(@RequestBody Category category);
 }
