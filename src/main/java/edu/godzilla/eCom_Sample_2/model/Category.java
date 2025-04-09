1cc23840f955addce500bd1369ac253f1078fac0
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-09 20:56:00 +0530

    Decorated using @Data, @AllArgsConstructor and @NoArgsConstructor annotations

--- src/main/java/edu/godzilla/eCom_Sample_2/model/Category.java
+++ src/main/java/edu/godzilla/eCom_Sample_2/model/Category.java
@@ -1,28 +1,13 @@
 package edu.godzilla.eCom_Sample_2.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
     private Long id;
     private String name;
-
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
