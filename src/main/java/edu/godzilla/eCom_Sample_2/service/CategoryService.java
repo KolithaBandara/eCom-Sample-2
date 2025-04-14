3a5ea4d3c46bec7f403fcb16d86017287722e321
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-14 10:40:27 +0530

    Changed the return type

--- src/main/java/edu/godzilla/eCom_Sample_2/service/CategoryService.java
+++ src/main/java/edu/godzilla/eCom_Sample_2/service/CategoryService.java
@@ -1,8 +1,10 @@
 package edu.godzilla.eCom_Sample_2.service;
 
 import edu.godzilla.eCom_Sample_2.model.Category;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -10,6 +12,6 @@ public interface CategoryService {
     List<Category> getAllCategories();
     String createCategory(@RequestBody Category category);
     String deleteCategory(Long categoryId);
-    String updateCategory(@PathVariable Long categoryId,
-                          @RequestBody Category category);
+    Category updateCategory(@PathVariable Long categoryId,
+                                           @RequestBody Category category);
 }
