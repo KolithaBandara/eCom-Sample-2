366cc61f55008b65f8829bc1fc59cb20dfb9c5cb
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2025-04-14 09:51:56 +0530

    Changed the return statement

--- src/main/java/edu/godzilla/eCom_Sample_2/controller/CategoryController.java
+++ src/main/java/edu/godzilla/eCom_Sample_2/controller/CategoryController.java
@@ -35,7 +35,7 @@ public class CategoryController {
     @PutMapping("/admin/category/update/{categoryId}")
     public String updateCategory(@PathVariable Long categoryId,
                                  @RequestBody Category category){
-        return "Category updated!";
+        return categoryService.updateCategory(categoryId, category);
     }
 
 }
